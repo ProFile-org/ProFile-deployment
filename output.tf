9519d5f9d701f3c94a6e0ab0c72d545c083276de
kaitoz11 <43519768+kaitoz11@users.noreply.github.com>
2023-06-03 23:46:51 +0700

    Base: add base configs for terraform

---
+++ output.tf
@@ -0,0 +1,3 @@
+output "public_ip" {
+    value = profileorg.ipv4_address
+}
---
+++ provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+variable "do_token" {}
+
+variable "my_ssh_key" {}
+
+provider "digitalocean" {
+  token = var.do_token
+}
