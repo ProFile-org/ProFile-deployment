ebd27cdc93d1317f6079ef517e01989ac5ec96e2
kaitozu <43519768+kaitoz11@users.noreply.github.com>
2023-07-07 22:47:14 +0700

    build: Apply changes (#8)
    * refactor: code base dev
    
    * fix: stupid output...
    
    * feat: add cloudflare for domain name configs
    
    * feat: Refactor structure and add ansible playbook for deploying
    
    * feat: Add basic auth (#7)
    
    * feat: Add basic auth
    
    * fix: fmt terraform
    
    * feat: monitoring with Prometheus-Grafana
    
    * refactor: dns records
    
    * feat: add traefik dashboard
    
    * feat: add labels for easier query

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -5,8 +5,11 @@ locals {
     region = "sgp1"
     size   = "s-4vcpu-8gb"
     dns_record = {
-      base = "dev-profileorg"
-      api  = "api-dev-profileorg"
+      base       = "dev-profileorg"
+      api        = "api-dev-profileorg"
+      traefik    = "traefik-dev-profileorg"
+      prometheus = "prom-dev-profileorg"
+      grafana    = "grafana-dev-profileorg"
     }
   }
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -16,19 +16,11 @@ resource "digitalocean_droplet" "profileorg_dev" {
   ssh_keys = [digitalocean_ssh_key.profileorg.fingerprint]
 }
 
-resource "cloudflare_record" "profileorg_dev" {
+resource "cloudflare_record" "profile" {
+  for_each   = local.instance.dns_record
   depends_on = [digitalocean_droplet.profileorg_dev]
   zone_id    = var.zone_id
-  name       = local.instance.dns_record.base
-  value      = digitalocean_droplet.profileorg_dev.ipv4_address
-  type       = "A"
-  proxied    = true
-}
-
-resource "cloudflare_record" "profileorg_dev_api" {
-  depends_on = [digitalocean_droplet.profileorg_dev]
-  zone_id    = var.zone_id
-  name       = local.instance.dns_record.api
+  name       = each.value
   value      = digitalocean_droplet.profileorg_dev.ipv4_address
   type       = "A"
   proxied    = true
